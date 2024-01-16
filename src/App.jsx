4a5a8fb841f40c660b5c050a69259cd220817638
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2024-01-17 01:23:30 +0800

    Deleted template

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import About from './components/About'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
-import Navmenu from './components/Navmenu'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
-import Template from './components/Template'
-import './App.css'
+import './styles/App.css'
 
 export default function App() {
   return (
@@ -17,7 +15,6 @@ export default function App() {
       <Skills />
       <Projects />
       <Footer />
-      <Template />
     </>
   )
 }
